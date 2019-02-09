7f826e8bbff165cd6f0337533d5663934373127f
BM HotLoudDusty <burningman@pi>
2019-02-09 20:50:48 +0000

    added hot

---
+++ hotlouddusty/hot.py
@@ -0,0 +1,31 @@
+import Adafruit_DHT
+
+from helpers import write_to_file, run_thread_loop
+
+PIN = 23
+HOT_FILEPATH = '../data/hot.csv'
+
+class Hot():
+
+    def __init__(self):
+        self.sensor = Adafruit_DHT.DHT22
+        self.pin = PIN
+
+    def record(self):
+        humidity, temperature = Adafruit_DHT.read_retry(
+            self.sensor, self.pin
+        )
+        if humidity is not None and temperature is not None:
+            print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
+            write_to_file(
+                HOT_FILEPATH,
+                round(temperature, 2),
+                round(humidity, 2),
+                ["temperature", "humidity"],
+            )
+        else:
+            print('Failed to get reading. Try again!')
+
+
+if __name__ == '__main__':
+    run_thread_loop(Hot().record, loop_secs=600000)
--- hotlouddusty/startup.sh
+++ hotlouddusty/startup.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 adb forward tcp:20175 tcp:50000
 pkill screen
-screen -dmS dusty bash -c 'cd ~/hotlouddusty/hotlouddusty; python dusty.py'
+screen -dmS hot bash -c 'cd ~/hotlouddusty/hotlouddusty; python hot.py'
 screen -dmS loud bash -c 'cd ~/hotlouddusty/hotlouddusty; python loud.py'
+screen -dmS dusty bash -c 'cd ~/hotlouddusty/hotlouddusty; python dusty.py'
 screen -dmS where bash -c 'cd ~/hotlouddusty/hotlouddusty; python where.py'
