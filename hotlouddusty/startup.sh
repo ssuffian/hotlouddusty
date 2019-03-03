840223315be7dd008407852f4d280fe9464f5c62
Hot Loud Dusty <hotlouddusty@burningman>
2019-03-03 14:54:45 -0800

    Updated

--- hotlouddusty/startup.sh
+++ hotlouddusty/startup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 adb forward tcp:20175 tcp:50000
 pkill screen
-screen -dmS hot bash -c 'cd ~/hotlouddusty/hotlouddusty; python hot.py'
-screen -dmS loud bash -c 'cd ~/hotlouddusty/hotlouddusty; python loud.py'
-screen -dmS dusty bash -c 'cd ~/hotlouddusty/hotlouddusty; python dusty.py'
-screen -dmS where bash -c 'cd ~/hotlouddusty/hotlouddusty; python where.py'
+screen -dmS hot bash -c 'cd ~/hotlouddusty/hotlouddusty; python3 hot.py'
+screen -dmS loud bash -c 'cd ~/hotlouddusty/hotlouddusty; python3 loud.py'
+screen -dmS dusty bash -c 'cd ~/hotlouddusty/hotlouddusty; python3 dusty.py'
+screen -dmS where bash -c 'cd ~/hotlouddusty/hotlouddusty; python3 where.py'
